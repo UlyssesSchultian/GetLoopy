04c05f4ea80e5187884ea2bbdb27744fec12172f
Ulysses Schultian <schultuj@mail.uc.edu>
2025-10-19 20:05:40 -0400

    Init

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,6 +1,6 @@
 public class NestedLoops {
     public static void main(String[] args) {
-        // Task 5: Triangle growing
+
         System.out.println("Task 5:");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
@@ -9,7 +9,7 @@ public class NestedLoops {
             System.out.println();
         }
 
-        // Task 6: Triangle shrinking
+
         System.out.println("\nTask 6:");
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
@@ -18,7 +18,7 @@ public class NestedLoops {
             System.out.println();
         }
 
-        // Task 7: Square
+
         System.out.println("\nTask 7:");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
